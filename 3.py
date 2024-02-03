9b48650c8ac0ecf9df288f721d0f0e9ee4fa8f3e
TheMerret <merret@bk.ru>
2024-02-03 15:42:22 +0300

    feat: add 3

---
+++ 3.py
@@ -0,0 +1,46 @@
+import csv
+
+
+def main():
+    """
+    выводи игры с участием персонажа
+    :return:
+    """
+    with open("game.txt", "r", encoding="utf8") as fd:
+        reader = csv.DictReader(fd, delimiter='$')
+        next(reader)
+        reader = list(reader)
+    reader.sort(key=lambda x: x["characters"])
+    while True:
+        req = input()
+        if req == "game":
+            break
+        character = req
+        l = 0
+        r = len(reader)
+        while l < r:
+            m = (l + r) // 2
+            if reader[m]['characters'] < character:
+                l = m + 1
+            else:
+                r = m
+        lower = l
+        l = 0
+        r = len(reader)
+        while l < r:
+            m = (l + r) // 2
+            if reader[m]['characters'] > character:
+                r = m
+            else:
+                l = m + 1
+        upper = l
+        games = reader[lower:upper]
+        if games:
+            titles = [i["GameName"] for i in games]
+            print(*titles[:5], sep=", ")
+        else:
+            print("Этого персонажа не существует")
+
+
+if __name__ == '__main__':
+    main()
